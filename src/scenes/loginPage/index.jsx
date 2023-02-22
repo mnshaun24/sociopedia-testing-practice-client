0a85f55143d5681cf2fd1aa3e8a3d3e4432c0431
Shaun Sticka <shaun.sticka@outlook.com>
2023-02-22 12:00:06 -0600

    login form layout done next setup API

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -1,7 +1,41 @@
-
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const LoginPage = () => {
-    return (<div>loginpage</div>)
+  const theme = useTheme();
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+
+  return (
+    <Box>
+      <Box
+        width="1000%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+          onClick={() => navigate("/home")}
+        >
+          Sociopedia
+        </Typography>
+      </Box>
+      <Box
+      width={isNonMobileScreen ? "50%" : "93%"}
+      p="2rem"
+      m="2rem auto"
+      borderRadius="1.5rem"
+      backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+            Welcome to the Test Website for Sociopedia! Happy Testing!
+        </Typography>
+
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
